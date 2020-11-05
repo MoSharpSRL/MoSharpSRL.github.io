9f1645c55eba76d3d27b094ae133e7dc16eddcd0
MoSharpSRL <73507113+MoSharpSRL@users.noreply.github.com>
2020-11-04 23:24:11 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-function toggle(){
-    var header = document.getElementById("header")
-    header.classList.toggle('active')
-}
-
 Swal.fire({
     title: 'Atencion',
     text: "El contenido de esta pagina Web es simplemente para una base estudiantil. Dicha pagina no tiene fines de lucrativos",
